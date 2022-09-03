96a8b3c6ea45f7d019359fd6edb47ee5e6615806
João Paulo Nunes <jpnune@gmail.com>
2022-09-03 17:11:04 -0300

    mais um

--- django1/settings.py
+++ django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+(*okw9l*k9-k1e$u&(*vwly@t=9fv!&-6kr@$ivd$9t#!oz*v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['git.heroku.com/django1-jpn.git']
 
 
 # Application definition
