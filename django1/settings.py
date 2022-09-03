263c3e49502292fc31c51c339d5c635cc8168144
João Paulo Nunes <jpnune@gmail.com>
2022-09-03 14:54:17 -0300

    liberado o allowed

--- django1/settings.py
+++ django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+(*okw9l*k9-k1e$u&(*vwly@t=9fv!&-6kr@$ivd$9t#!oz*v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jpnune.pythonanywhere.com']
 
 
 # Application definition
